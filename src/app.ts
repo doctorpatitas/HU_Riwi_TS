dbfe3c8974a2e58332ef49609d6b71a31743ea8a
Lneiras <lauraneira62@gmail.com>
2026-08-14 18:44:08 -0500

    Las importaciones de las rutas a app completadas

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,20 @@
 import express from 'express';
 import 'dotenv/config';
 import { ConnectDB } from './config/db.js'
+import tlRouter from './routes/tl.routes.js';
+import trackRouter from './routes/track.routes.js';
+import clanRouter from './routes/clan.routes.js';
+import coderRouter from './routes/coder.routes.js';
 
 const {PORT} = process.env
 
 const app = express();
 app.use(express.json());
+
+app.use('/tl', tlRouter);
+app.use('/track', trackRouter);
+app.use('/clan', clanRouter);
+app.use('/coder', coderRouter);
 
 app.listen(PORT, async() => {
     await ConnectDB();
